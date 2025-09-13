e6e345e1680cb88eb0e7a322664ffa3d764f092c
Valeryi Zaletskyi <vzalecki@gmail.com>
2025-09-13 11:30:54 +0300

    fix type Movie

--- src/types/movie.ts
+++ src/types/movie.ts
@@ -1,13 +1,16 @@
-export interface MoviesResponse {
-  page: number;
-  results: Movie[];
-  total_pages: number;
-  total_results: number;
-}
-
 export interface Movie {
   id: number;
   title: string;
   overview: string;
   poster_path: string;
+  backdrop_path?: string;
+  release_date?: string;
+  vote_average?: number;
+}
+
+export interface MoviesResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
 }
