486ef0ef37d3ec8e19c79f74ba94ad0f48fd066a
Valeryi Zaletskyi <vzalecki@gmail.com>
2025-09-14 22:21:00 +0300

    fix movieServise

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,21 +1,17 @@
 import axios from 'axios';
 import type { Movie } from '../types/movie';
 
-// Базовий URL
 const API_URL = 'https://api.themoviedb.org/3';
 
-// Токен з .env (v4 API Read Access Token)
 const token = import.meta.env.VITE_TMDB_TOKEN;
 if (!token) {
   throw new Error('TMDB Bearer token is missing! Add VITE_TMDB_TOKEN to your .env');
 }
 
-// Налаштування axios
 axios.defaults.baseURL = API_URL;
 axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
 axios.defaults.headers.common['accept'] = 'application/json';
 
-// ✅ Інтерфейс має бути тут
 export interface MoviesResponse {
   page: number;
   results: Movie[];
@@ -23,7 +19,6 @@ export interface MoviesResponse {
   total_results: number;
 }
 
-// ✅ Функція для пошуку фільмів
 export const fetchMovies = async (query: string, page: number = 1): Promise<MoviesResponse> => {
   if (!query.trim()) {
     return {
